6192bc5e3abffa1435def1e54ea4c1d74d2bb3d0
drosskopf <drosskopf@ldschurch.org>
2017-03-17 00:11:03 -0600

    Lesson 4

---
+++ Src/Controllers/FamilyController.cs
@@ -0,0 +1,14 @@
+using Microsoft.Extensions.Options;
+
+namespace webapp{
+    public class FamilyController{
+        private IOptions<Family> _family {get;set;}
+
+        public FamilyController(IOptions<Family> family){
+                _family=family;
+        }
+        public Family Get(){
+             return _family.Value;
+        }
+    }
+}
---
+++ Src/Startup.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace webapp
+{
+   public class Startup{
+        public IConfigurationRoot Configuration {get;}
+        public Startup(IHostingEnvironment env)
+        {
+            //Environment Development, Staging, Production
+            //Powershell $env:ASPNETCORE_ENVIRONMENT="Development"
+            //IOS EXPORT ASPNETCORE_ENVIRONMENT="Development"
+            //CMD Setx ASPNETCORE_ENVIRONMENT="Development"
+            var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .AddJsonFile($"appsettings_{env.EnvironmentName}.json",true);
+            Configuration = config.Build();
+            
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOptions();
+            services.Configure<Family>(Configuration.GetSection("Family"));
+            services.AddMvc();
+        }
+        public void Configure(IApplicationBuilder app, IOptions<Family> family)
+        {
+            var dad = family.Value.Dad;
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseMvc(route=>{
+                route.MapRoute(name:"Default",
+                template:"{controller=Home}/{action=Index}/{id?}"
+                );
+            }
+            );
+        
+        }
+    }
+}
